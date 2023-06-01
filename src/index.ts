0e32bb995772d9905fb13f8e1b5b6f1ecec4da4a
Shashi Sharma <shashi.sharma1@publicissapient.com>
2023-06-01 20:21:12 +0530

    server setuped

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello CSS Gradient Generator");
+(function () {
+  const body = document.body;
+
+  const title: HTMLHeadingElement = document.createElement("h1");
+
+  title.textContent = "Hello CSS Gradient Generator";
+
+  body.appendChild(title);
+})();
